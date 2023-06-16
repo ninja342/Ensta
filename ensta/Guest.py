a567398b8dbcaa7591c1df21974cf2e89d79b959
Ninja342 <tarik751@gmail.com>
2023-06-16 15:06:35 +0300

    Update Guest.py

--- ensta/Guest.py
+++ ensta/Guest.py
@@ -114,9 +114,8 @@ class Guest:
                 headers=request_headers
             )
             response_json: dict = http_response.json()
-            print(response_json)
             print(http_response.text)
-
+            
             if "status" in response_json:
                 if response_json["status"] == "ok" and "data" in response_json:
                     if "user" in response_json["data"]:
